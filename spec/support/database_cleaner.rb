4a8e33a8b02f65e90b88b17dc6123b4e6549df92
comercial-rgb <comercial@instasolutions.com.br>
2026-01-27 04:42:53 -0300

    Fix: Disable DatabaseCleaner config - gem not in Gemfile

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,21 +1,25 @@
-RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.append_after(:each) do
-    DatabaseCleaner.clean
-  end
-end
+# DatabaseCleaner configuration - DISABLED
+# The database_cleaner gem is not included in the Gemfile.
+# Uncomment the gem in Gemfile and this code when needed for testing.
+#
+# RSpec.configure do |config|
+#   config.before(:suite) do
+#     DatabaseCleaner.clean_with(:truncation)
+#   end
+#
+#   config.before(:each) do
+#     DatabaseCleaner.strategy = :transaction
+#   end
+#
+#   config.before(:each, :js => true) do
+#     DatabaseCleaner.strategy = :truncation
+#   end
+#
+#   config.before(:each) do
+#     DatabaseCleaner.start
+#   end
+#
+#   config.append_after(:each) do
+#     DatabaseCleaner.clean
+#   end
+# end
